5a13421d13830d1ca8024afb622010f383d87a18
Zh0ngS0n <83699106+zer0qs@users.noreply.github.com>
2024-02-06 21:42:59 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,7 +1,7 @@
 const CONFIGDATA = {
   titleHeader: "Chào Thu, Anh có điều muốn nói. </br> Em Còn Giận Anh Lắm Phải Không!",
   descriptionHeader:
-    "Anh biết rằng trước giờ anh đã làm em tổn thương rất nhiều. Đôi khi anh vô tâm, \
+    "Anh biết rằng trước giờ anh đã làm em tổn thương rất nhiều. Đôi khi anh vô tâm, \r\n\
     không biết quan tâm đến cảm xúc của em. Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã Hết Dỗi.",
   buttonYes: "Đã Hết Rồi",
   buttonNo: "Chưa Còn Lâu",
