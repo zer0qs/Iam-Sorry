9fe5b25d8655348aef8cf8536bc63e82bccd38ec
Zh0ngS0n <83699106+zer0qs@users.noreply.github.com>
2024-02-07 19:48:23 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -2,8 +2,8 @@ const CONFIGDATA = {
   titleHeader: "Chào Thu, Anh có điều muốn nói. </br> Em Có yêu Anh Không!",
   descriptionHeader:
   "Anh yêu em nhiều lắm, xa em một ngày mà anh cảm giác nó lâu như 24h. Nói chung là không còn từ nào để diễn tả tình yêu anh dành cho em <3. EM CÓ YÊU ANH KHÔNG?",
-  buttonYes: "Không Còn Lâu!",
-  buttonNo: "Có, Rất Yêu Ạ!",
+  buttonYes: "Có, Rất Yêu Ạ!",
+  buttonNo: "Không Còn Lâu!",
   titleModar: " Ok em yêu.",
   descriptionModar:
     "Cảm ơn em nhé. </br> Anh sẽ hứa sẽ không làm em giận nữa. </br> Nhưng mà hên sui :((.</br> haha <3",
